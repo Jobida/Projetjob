1c3f83121e48063bb3c2e74f72c777ab56ee469c
jobida <jobida3115@gmail.com>
2022-12-18 00:17:32 -0600

    project17 array

---
+++ src/Seventeen.java
@@ -0,0 +1,16 @@
+public class Seventeen {
+    public static void main(String[] args){
+        int[] array1 ={4,20,15,2,19};
+        String[] array2 ={ "Hello","there","how","are","you"};
+        System.out.println((array1[2]));
+        System.out.println((array2[1]));
+        System.out.println(array1.length);
+        System.out.println(array2.length);
+        for(int i=0; i<array1.length; i++) {
+            System.out.println(array1[i]);
+        }
+        for(int j=0; j<array2.length; j++){
+            System.out.println(array2[j]);}
+
+    }
+}
